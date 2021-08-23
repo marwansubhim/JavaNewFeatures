f0502b4df8b734f51594555fae7f9ae2347d8ef3
Marwan Subhi <marwan.subhi4@gmail.com>
2021-08-23 12:53:12 -0400

    Added notes of Anonymous class VS Lambda Expressions

--- src/main/java/lambdaExpression/Less4.java
+++ src/main/java/lambdaExpression/Less4.java
@@ -14,10 +14,13 @@ public class Less4 {
             public int performOperation(int a, int b) {
                 return a + b;
             }
-        };
+        };// this is what they called Anonymous inner class which has more boilerplate code instead lambda is preferred only if we have one abstract method
 
         System.out.println("" +
                 "The sum of two input integers without lambda is: " + oldSum.performOperation(firstInt, secondInt));
+
+        // Here we are using the Lambda expression which shortened lots of boilerplate code compared to the above implementation
+        // Three expressions we didn't have to write lots of code, and we are able to achieve three different behaviors as shown below:
 
         ArithmeticOperation sum = (a, b) -> a + b;
         ArithmeticOperation sub = (a, b) -> a - b;
@@ -28,3 +31,15 @@ public class Less4 {
         System.out.println("The multiplication with lambda is: "+multi.performOperation(firstInt,secondInt));
     }
 }
+
+
+/**
+ * Anonymous Class                                                                                   vs                      Lambda Expression
+ * It's an anonymous inner class without a name                                                                   It's an anonymous method without a name
+ * It can implement the interfaces with any number of abstract methods inside them,                               It can implement the interface which has one method called Functional Interface
+ * They can be instantiated and can extend abstract and concrete classes                                          They can't be instantiated and can't extend abstract and concrete classes
+ * Instance variables can be declared and "this" inside it always refer to the current inner class                Only local variables can be declared and "this" inside it always refer to the outer enclosing class
+ * Memory isndie heap will be allocated on demand whenever we create an object for it                             Permanent memory (Method area) will be allocated for it
+ *
+ *
+ */
